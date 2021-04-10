e8b58ad40037830277b7ea10880d5f1e123b335b
Andrzej <andrzej.przybysz01@gmail.com>
2021-04-10 16:38:18 +0200

    add sample product to Flyway migration

---
+++ src/main/resources/db/migration/dev/V2__initDataFromPlaceholders.sql
@@ -0,0 +1,12 @@
+INSERT INTO currency (id, name, symbol) VALUES (1, '${currencies.pln.name}', '${currencies.pln.symbol}');
+INSERT INTO currency (id, name, symbol) VALUES (2, '${currencies.euro.name}', '${currencies.euro.symbol}');
+INSERT INTO currency (id, name, symbol) VALUES (3, '${currencies.dollar.name}', '${currencies.dollar.symbol}');
+
+INSERT INTO shop (id, name) VALUES (1, '${shops.lidl}');
+INSERT INTO shop (id, name) VALUES (2, '${shops.intermarche}');
+INSERT INTO shop (id, name) VALUES (3, '${shops.carrefour}');
+INSERT INTO shop (id, name) VALUES (4, '${shops.auchan}');
+INSERT INTO shop (id, name) VALUES (5, '${shops.netto}');
+INSERT INTO shop (id, name) VALUES (6, '${shops.aldi}');
+INSERT INTO shop (id, name) VALUES (7, '${shops.kaufland}');
+INSERT INTO shop (id, name) VALUES (8, '${shops.ikea}');
